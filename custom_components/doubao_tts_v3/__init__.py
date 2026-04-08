ee580216089094ec0b945caefb3ebd8fb79425f1
ifcc <ifccsmile@gmail.com>
2026-04-08 23:21:01 +0800

    init

---
+++ custom_components/doubao_tts_v3/__init__.py
@@ -0,0 +1,14 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setup(entry, "tts")
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "tts")
---
+++ custom_components/doubao_tts_v3/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "doubao_tts_v3"
+
+WS_URL = "wss://openspeech.bytedance.com/api/v3/tts/unidirectional/stream"
+
+DEFAULT_SPEAKER = "zh_female_shuangkuaisisi_moon_bigtts"
+DEFAULT_FORMAT = "mp3"
+DEFAULT_SAMPLE_RATE = 24000
+DEFAULT_SPEECH_RATE = 0
+DEFAULT_LOUDNESS_RATE = 0
+
+RESOURCE_ID = "seed-tts-2.0"
